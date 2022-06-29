ccbca738f79af70d6139639516dda94b8739bdc5
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-29 17:46:50 -0600

    commit tree 8091efd75cd008b251cbeb1493fe94186b831d0c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 8, 'M', 'brown', 'spicy', 1); 
+INSERT INTO bears VALUES (2, 'Rowdy', 3,'M', 'blue', 'loud', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 23, 'F', 'purple', 'slow', 0); 
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 13, 'M', 'brown', 'bold', 1); 
+INSERT INTO bears VALUES (5, 'Melissa', 6, 'F', 'pink', 'silly', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 25, 'M', 'green', 'grumpy', 0); 
+INSERT INTO bears VALUES (7, 'Wendy', 33, 'M', 'red', 'soft', 0);
+INSERT INTO bears VALUES (8, NULL, 1, 'M', 'black', 'timid', 1);
